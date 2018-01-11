117e59fd085ea79d5267d00e4e3848e0dd8536ae
Lars Helge Øverland <lars@dhis2.org>
2018-01-11 18:02:40 +0100

    Releases

--- play/scripts/reinit-instance-v2.sh
+++ play/scripts/reinit-instance-v2.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $# -eq 0 ]; then
-  echo -e "Usage: $0 <instance>\n"
+  echo -e "Usage: $0 <instances...>\n"
   exit 1
 fi
 
@@ -22,8 +22,10 @@ function downloadWar() {
   wget --progress=bar https://s3-eu-west-1.amazonaws.com/releases.dhis2.org/$1/dhis.war -O /ebs1/instances/$1/tomcat/webapps/$1.war
 }
 
-validate $1
-$DIR/stop-instance.sh $1
-cleanWebApps $1
-downloadWar $1
-$DIR/start-instance.sh $1
+for instance in $@; do
+  validate $instance
+  $DIR/stop-instance.sh $instance
+  cleanWebApps $instance
+  downloadWar $instance
+  $DIR/start-instance.sh $instance
+done
