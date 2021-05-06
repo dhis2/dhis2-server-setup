dcdceff7c29f3cb9f08ed89110a6067ccc11d19d
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2021-05-06 15:09:25 +0200

    chore: move javadoc location on S3

--- ci/scripts/sync-javadocs-s3.sh
+++ ci/scripts/sync-javadocs-s3.sh
@@ -2,7 +2,7 @@
 
 DOC_LOCATION="/ebs1/home/jenkins/jobs/${JOB_NAME}/javadoc/."
 # the second argument is the branch (suffixed with '/'), in the case that $1 is a tag
-S3_LOCATION="s3://docs.dhis2.org/$1/javadoc/"
+S3_LOCATION="s3://docs.dhis2.org/javadoc/$1/"
 
 # Copy doc directories to S3
 ~/.local/bin/aws s3 sync $DOC_LOCATION $S3_LOCATION
