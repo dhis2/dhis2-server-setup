120bf2bcb4589804094e01659339c039079f5b78
radnov <radnov@pm.me>
2021-12-02 16:56:53 +0200

    build: Update javadoc sync script for new Jenkins [DEVOPS-114]

--- ci/scripts/sync-javadocs-s3.sh
+++ ci/scripts/sync-javadocs-s3.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-DOC_LOCATION="/ebs1/home/jenkins/jobs/${JOB_NAME}/javadoc/."
+set -euo pipefail
+
+DOC_LOCATION="${JENKINS_HOME}/jobs/${JOB_NAME}/javadoc/."
 # the second argument is the branch (suffixed with '/'), in the case that $1 is a tag
-S3_LOCATION="s3://docs.dhis2.org/javadoc/$1/"
+S3_LOCATION="s3://test-docs.dhis2.org/javadoc/$1/"
 
 # Copy doc directories to S3
-~/.local/bin/aws s3 sync $DOC_LOCATION $S3_LOCATION
-
+aws s3 sync $DOC_LOCATION $S3_LOCATION
