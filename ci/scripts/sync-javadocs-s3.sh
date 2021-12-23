1956cb9a8ea97ad785adaf90721cf19c84fce7be
radnov <radnov@pm.me>
2021-12-23 15:19:09 +0200

    build: Change javadoc location to agent workspace [DEVOPS-114]

--- ci/scripts/sync-javadocs-s3.sh
+++ ci/scripts/sync-javadocs-s3.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-DOC_LOCATION="${JENKINS_HOME}/jobs/${JOB_NAME}/branches/$1/javadoc/."
+DOC_LOCATION="${WORKSPACE}/dhis-2/target/site/apidocs"
 # the second argument is the branch (suffixed with '/'), in the case that $1 is a tag
 S3_LOCATION="s3://test-docs.dhis2.org/javadoc/$1/"
 
