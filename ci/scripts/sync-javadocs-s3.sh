2875afa1df28df89beda407b378552c23b4e1c7d
radnov <radnov@pm.me>
2021-12-23 15:26:09 +0200

    build: Remove progress output from s3 cmd [DEVOPS-114]

--- ci/scripts/sync-javadocs-s3.sh
+++ ci/scripts/sync-javadocs-s3.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 DOC_LOCATION="${WORKSPACE}/dhis-2/target/site/apidocs"
 # the second argument is the branch (suffixed with '/'), in the case that $1 is a tag
-S3_LOCATION="s3://test-docs.dhis2.org/javadoc/$1/"
+S3_LOCATION="s3://docs.dhis2.org/javadoc/$1/"
 
 # Copy doc directories to S3
-aws s3 sync $DOC_LOCATION $S3_LOCATION
+aws s3 sync --no-progress $DOC_LOCATION $S3_LOCATION
