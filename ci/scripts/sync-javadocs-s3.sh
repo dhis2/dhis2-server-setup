fd46559e68a1ba15e490034a1cbb53bd9673dea6
radnov <radnov@pm.me>
2021-12-23 14:41:04 +0200

    build: Update javadocs location for multibranch pipeline [DEVOPS-114]

--- ci/scripts/sync-javadocs-s3.sh
+++ ci/scripts/sync-javadocs-s3.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-DOC_LOCATION="${JENKINS_HOME}/jobs/${JOB_NAME}/javadoc/."
+DOC_LOCATION="${JENKINS_HOME}/jobs/${JOB_NAME}/branches/$1/javadoc/."
 # the second argument is the branch (suffixed with '/'), in the case that $1 is a tag
 S3_LOCATION="s3://test-docs.dhis2.org/javadoc/$1/"
 
